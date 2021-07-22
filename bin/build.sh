11f02d2c495b462f59a85551526dc12c90263648
Dennis Walker <denniswalker@me.com>
2021-07-22 07:10:17 -0600

    Adds a build script to simplify the build process.

---
+++ bin/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+if [[ -z $1 ]]; then
+  echo "Please specify a branch to checkout, e.g. 'bin/build.sh release/1.0'"
+  exit 1
+fi
+
+cd $THIS_DIR/..
+
+echo "Cloning into docs-csm..."
+rm -rf docs-csm
+git clone git@github.com:Cray-HPE/docs-csm.git
+response=$(cd ./docs-csm && git checkout "$1")
+echo $response
+
+echo "Making markdown files friendly to Hugo..."
+docker-compose run hugo_prep
+
+cd $OLDPWD
