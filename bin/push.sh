c099792d998d65d2a454337d95438ee8d0a75a1a
Michael Tupitsyn <mikhail.tupitsyn@hpe.com>
2021-11-16 10:37:15 -0800

    Use shallow clone of specific branch before push

--- bin/push.sh
+++ bin/push.sh
@@ -4,11 +4,8 @@ set -ex
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir -p docs-csm
 cd docs-csm
-git clone git@github.com:Cray-HPE/docs-csm.git
-cd docs-csm
-git_sha=$(git log -1 --format=%h)
-git checkout -b release/docs-html -t origin/release/docs-html || git checkout --orphan release/docs-html
-cd ../..
+git clone --depth=1 -b release/docs-html git@github.com:Cray-HPE/docs-csm.git
+cd ..
 rm -rf docs-csm/docs-csm/* docs-csm/docs-csm/.github docs-csm/docs-csm/.gitignore docs-csm/docs-csm/.version
 cp -r public/* docs-csm/docs-csm/
 cd docs-csm/docs-csm
